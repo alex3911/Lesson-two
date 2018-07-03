cfefc64329eced49855eed1d1f41295d4ef44662
Alex <alex3911@mail.ru>
2018-07-03 18:43:35 +0300

    Bugfix

--- LessonTwoJobFour.java
+++ LessonTwoJobFour.java
@@ -3,15 +3,13 @@ import java.util.Random;
 public class LessonTwoJobFour {
 	public static void main (String args []) {
 		Random rand = new Random();
-		int num = random.nextInt(100);
-		System.out.println(num);
 		
+		int num = rand.nextInt(1000);
+		if (num % 100 >=11 & num % 100 <= 14) System.out.println( num + " рублей ");
+		else if (num % 10 == 1) System.out.println( num + " рубль ");
+		else if (num % 10 >=2 & num % 10 <= 4) System.out.println( num + " рубля ");
+		else System.out.println( num + " рублей ");
 		
-	
-	
-	
-	
-		
-		
+			
 	}
 }
